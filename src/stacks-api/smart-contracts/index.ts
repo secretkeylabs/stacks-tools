b138ee43a411e02a1e8034e151476504f9869341
Eduard Bardají Puig <ebardaji@gmail.com>
2024-10-30 18:03:47 +0000

    Move read to stacks-rpc-api

--- src/stacks-api/smart-contracts/index.ts
+++
@@ -1,6 +0,0 @@
-import { readOnly } from "./read-only.js";
-export type * as ReadOnly from "./read-only.js";
-
-export const smartContracts = {
-  readOnly,
-};
--- src/stacks-rpc-api/smart-contracts/index.ts
+++ src/stacks-rpc-api/smart-contracts/index.ts
@@ -1,6 +1,9 @@
 import { mapEntry } from "./map-entry.js";
 export type * as MapEntry from "./map-entry.js";
+import { readOnly } from "./read-only.js";
+export type * as ReadOnly from "./read-only.js";
 
 export const smartContracts = {
   mapEntry,
+  readOnly,
 };
