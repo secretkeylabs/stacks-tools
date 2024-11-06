c6fc9a013a8d27cc1fbd4b99450895ff9f6c613a
Eduard Bardají Puig <ebardaji@gmail.com>
2024-11-06 18:41:32 +0000

    Add get-check-delegation

--- src/pox4-api/read-only/index.ts
+++ src/pox4-api/read-only/index.ts
@@ -1,6 +1,10 @@
 import { getStackerInfo } from "./get-stacker-info.js";
 export type * as GetStackerInfo from "./get-stacker-info.js";
 
+import { getCheckDelegation } from "./get-check-delegation.js";
+export type * as GetCheckDelegation from "./get-check-delegation.js";
+
 export const readOnly = {
   getStackerInfo,
+  getCheckDelegation,
 };
